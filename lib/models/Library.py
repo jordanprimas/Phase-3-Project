f2b250a6b2ee89aaca896835100c1fa93ad22ce1
Jordan Primas <jorprimas20@gmail.com>
2024-01-31 19:06:31 -0800

    adding library class methods

--- lib/models/Library.py
+++ lib/models/Library.py
@@ -1,14 +1,20 @@
+from __init__ import CURSOR, CONN 
+import ipdb
+
 class Library:
-    def __init__(self, name, address):
+    all = {}
+
+    def __init__(self, name, zip_code, id=None):
+        self.id = id 
         self.name = name 
-        self.address = address
+        self.zip_code = zip_code
 
     @property
     def name(self):
         return self._name
     
     @name.setter
-    def name(self):
+    def name(self, name):
         if isinstance(name, str) and len(name):
             self._name = name 
         else:
@@ -17,14 +23,131 @@ class Library:
             )
 
     @property
-    def address(self):
-        return self._address
+    def zip_code(self):
+        return self._zip_code
 
-    @address.setter
-    def address(self, address):
-        if isinstance(address, (str, int)) and len(address):
-            self._address = address
+    @zip_code.setter
+    def zip_code(self, zip_code):
+        zip_code_string = str(zip_code)
+        if isinstance(zip_code, int) and len(zip_code_string) == 5:
+            self._zip_code = zip_code
         else:
             raise ValueError(
-                "Address must be a string or integer with at least 1 character."
+                "Zip code must be an integer with 5 characters."
             )
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS libraries (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            zip_code INTEGER)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+    
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS libraries
+        """
+        CURSOR.execute(sql)
+        CONN.commit() 
+
+    def save(self):
+        sql = """
+            INSERT INTO libraries (name, zip_code)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.zip_code))
+        CONN.commit()
+
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
+    
+    @classmethod
+    def create(cls, name, zip_code):
+        library = cls(name, zip_code)
+        library.save()
+        return library
+
+    def update(self):
+        sql = """
+            UPDATE libraries
+            SET name = ?, zip_code = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.zip_code, self.id))
+        CONN.commit() 
+    
+    def delete(self):
+        sql = """
+            DELETE FROM libraries
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
+    
+    @classmethod
+    def instance_from_db(cls, row):
+        library = cls.all.get(row[0])
+        if library:
+            library.name = row[1]
+            library.zip_code = row[2]
+        else:
+            library = cls(row[1], row[2])
+            library.id = row[0]
+            cls.all[library.id] = library
+        return library
+    
+    @classmethod
+    def get_all(cls):
+        sql = """
+            SELECT * FROM libraries
+        """
+        rows = CURSOR.execute(sql).fetchall()
+
+        return [cls.instance_from_db(row) for row in rows]
+
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """
+            SELECT *
+            FROM libraries 
+            WHERE name = ?
+            LIMIt 1
+        """
+        row = CURSOR.execute(sql, (name,)).fetchone()
+
+        if not row:
+            return None 
+        return cls.instance_from_db(row) 
+
+    @classmethod
+    def find_by_id(cls, id):
+        sql = """
+            SELECT * 
+            FROM libraries
+            WHERE id = ?
+            LIMIT 1
+        """
+        row = CURSOR.execute(sql, (id,)).fetchone()
+
+        if not row:
+            return None 
+        return cls.instance_from_db(row)
+    
+    def books(self):
+        from book import Book
+        sql = """
+            SELECT * FROM books
+            WHERE library_id = ?
+        """
+        CURSOR.execute(sql, (self.id,))
+        
+        rows = CURSOR.fetchall()
+        return [Book.instance_from_db(row) for row in rows]
+        
+
+ipdb.set_trace()
